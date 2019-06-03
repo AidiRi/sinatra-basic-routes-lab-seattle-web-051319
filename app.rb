f612b96ab5c81c1275762da2688a43493fdb9cbf
AidiRi <rivera.aidi@gmail.com>
2019-06-03 08:51:26 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Aidi!"
+  end
+
+  get '/hometown' do
+    "My hometown is St. Petersburg."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 40 Day Dream."
+  end
+
 end
